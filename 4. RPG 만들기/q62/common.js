d2bcc570fbce40d053c25fc563a59c9a00a49489
김상어 <rjsgh1108@naver.com>
2025-04-09 17:35:11 +0900

    Add files via upload

---
+++ 4. RPG 만들기/q62/common.js
@@ -0,0 +1,43 @@
+// 자주쓰는것들을 함수로 만들어 놓음
+// 나중에는 <script src="common.js"> 하나만 더 넣으면 됨
+// 순서가 중요하니 가능하면 맨 위에 적을것 (css/common/js 순이면 됨)
+
+// 엔터
+function br(){
+    document.write("<br>")
+}
+// 가로로 긴 줄
+function hr(){
+    document.write("<hr>")
+}
+// document.write를 쓰면 html이 싹 날아가서 dw를 쓰지 않는 방향으로 변경
+function tx(str){
+    var screenString = screenMessageBox.value;
+    screenString = screenString + str;
+    screenMessageBox.value = screenString;
+}
+
+function txPlayerInfo(str){
+    var screenString = screenPlayerInfo.value;
+    // screenString = screenString + str;
+    // screenPlayerInfo.value =screenString;
+    screenPlayerInfo.value = str;
+}
+
+function txGameObject(str){
+    var screenString = screenGameObject.value;
+    screenGameObject.value =str;
+}
+
+// 텍스트 지우기
+function txClear(){
+    screenMessageBox.value = "";
+}
+
+function txPlayerInfoClear(){
+    screenPlayerInfo.value = "";
+}
+
+function txGameObjectClear(){
+    screenGameObject.value = "";
+}
